a5997988b293cc6e2ffad971ac2e6daab76d3a4b
Jason Zheng <jzz18@ic.ac.uk>
2023-09-27 17:33:16 +0100

    Implement SetWord()

--- pkg/devices/memory.go
+++ pkg/devices/memory.go
@@ -15,9 +15,21 @@ func NewMemory() *Memory {
 }
 
 func (m *Memory) SetWord(addr, value uint32) error {
-	if addr > 0x10000 {
+	if addr > memorySize {
 		return errors.New("SetWord() tried to access invalid memory address")
 	}
-	m.memory[addr] = value
+
+	// Aligned on 4-byte boundary
+	if addr%4 == 0 {
+		m.memory[addr/4] = value
+	} else {
+		lowerIndex := addr / 4
+		upperIndex := lowerIndex + 1
+		lowerByteShift := (addr % 4) * 8
+		upperByteShift := 32 - lowerByteShift
+
+		m.memory[lowerIndex] &= (value << lowerByteShift) | (0xFFFFFFFF >> upperByteShift)
+		m.memory[upperIndex] &= (value >> upperByteShift) | (0xFFFFFFFF << lowerByteShift)
+	}
 	return nil
 }
