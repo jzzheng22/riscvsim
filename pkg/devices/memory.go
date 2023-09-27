32d3d2e47a22267672cc6f5ae74c5c99d1fb96d7
Jason Zheng <jzz18@ic.ac.uk>
2023-09-27 17:33:02 +0100

    Make memorySize constant

--- pkg/devices/memory.go
+++ pkg/devices/memory.go
@@ -6,17 +6,12 @@ type Memory struct {
 	memory []uint32
 }
 
+const memorySize = 0x10000
+
 func NewMemory() *Memory {
 	return &Memory{
-		memory: make([]uint32, 0x10000),
+		memory: make([]uint32, memorySize/4),
 	}
-}
-
-func (m *Memory) GetWord(addr uint32) (uint32, error) {
-	if addr > 0x10000 {
-		return 0, errors.New("GetWord() tried to access invalid memory address")
-	}
-	return m.memory[addr], nil
 }
 
 func (m *Memory) SetWord(addr, value uint32) error {
