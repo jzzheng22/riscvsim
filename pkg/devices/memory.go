69aca05178ad084bd09f7a61c0fda456132b32c8
Jason Zheng <jzz18@ic.ac.uk>
2023-09-27 17:33:36 +0100

    Implement GetWord()

--- pkg/devices/memory.go
+++ pkg/devices/memory.go
@@ -33,3 +33,22 @@ func (m *Memory) SetWord(addr, value uint32) error {
 	}
 	return nil
 }
+
+func (m *Memory) GetWord(addr uint32) (uint32, error) {
+	if addr > memorySize {
+		return 0, errors.New("GetWord() tried to access invalid memory address")
+	}
+	// Aligned on 4-byte boundary
+	if addr%4 == 0 {
+		return m.memory[addr/4], nil
+	} else {
+		lowerIndex := addr / 4
+		upperIndex := lowerIndex + 1
+		lowerByteShift := (addr % 4) * 8
+		upperByteShift := 32 - lowerByteShift
+		lowerWord := m.memory[lowerIndex] >> lowerByteShift
+		upperWord := m.memory[upperIndex] << upperByteShift
+		return upperWord | lowerWord, nil
+	}
+}
+
